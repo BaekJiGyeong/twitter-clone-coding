778f8849371065a4e678ab90bc81e2bf38d80f3c
백지경 <jigyeong.baek@kt.com>
2021-09-17 15:13:52 +0900

    fbase

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import AppRouter from "./Router";
+import { authService } from "../fbase";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
-   <AppRouter />
+    <>
+    <AppRouter isLoggedIn={isLoggedIn} />
+    <footer>&copy; twitter-clone-coding {new Date().getFullYear()}</footer>
+   </>
   );
 }
 
