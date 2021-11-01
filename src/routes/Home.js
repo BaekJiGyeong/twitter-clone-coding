4859dd326d2b5f4adc27bf7ec984c6bcd2ead6d0
BaekJiGyeong <wlrud0126@hanmail.net>
2021-11-01 19:27:28 +0900

    add : firebase realtime with snapshot

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "../fbase";
-import { addDoc, collection } from "@firebase/firestore";
+import { addDoc, collection, getDocs, onSnapshot, orderBy, query } from "firebase/firestore";
 
-const Home = () => {
+const Home = ({userObj}) => {
     const [nweet, setNweet] =  useState("");
+    const [nweets, setNweets] = useState([]);
+    // const getNweets = async() => {
+    //     const dbNweets = await getDocs(collection(dbService, "nweets"));
+    //     dbNweets.forEach((document)=>
+    //         {
+    //             const nweetObject={
+    //                 ...document.data(),
+    //                 id:document.id
+    //             };
+    //             setNweets((prev)=>[document.data(), ...prev]);
+    //         }
+    //     );
+    // }
+    useEffect(()=>{
+        const q = query(collection(dbService,"nweets"), orderBy("createdAt","desc"));
+        onSnapshot(q,(snapshot)=> {
+            const nweetArr = snapshot.docs.map((doc)=>({
+                id:doc.id,
+                ...doc.data()
+            }));
+            setNweets(nweetArr);
+        });
+    }, []);
     const onSubmit = async(event) => {
         event.preventDefault();
         const docRef = await addDoc(collection(dbService,"nweets"),{
-            nweet,
+            text:nweet,
             createdAt : Date.now(),
+            creatorId:userObj.uid
         });
         setNweet("");
     }
@@ -24,6 +48,12 @@ const Home = () => {
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
                 <input type="submit" value="Twitter"/>
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                <div key={nweet.id}>
+                    <h4>{nweet.text}</h4>
+                </div>))}
+            </div>
         </div>
     )
 };
