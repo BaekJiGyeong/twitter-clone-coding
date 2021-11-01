48140195159663010851d5b40fea400fef45ce27
BaekJiGyeong <wlrud0126@hanmail.net>
2021-11-01 15:33:12 +0900

    add : firebase storage upload

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
-import { initializeApp } from "firebase/app";
 import * as firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY ,
@@ -15,3 +16,4 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
   export const firebaseInstance = firebase;
   export const authService = getAuth();
+  export const dbService = getFirestore();
